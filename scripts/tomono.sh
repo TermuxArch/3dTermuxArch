530c43314e2f31aca548d02d86df35ee928f5737
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-02-03 11:07:13 -0500

    commit 186829032 on 20220203

--- scripts/tomono.sh
+++ scripts/tomono.sh
@@ -2,10 +2,10 @@
 # Copyright 2022 (c) by SDRausty, all rights reserved, see LICENSE
 # ffmpeg -i "$1" -vn -ar 11025 -ac 2 -ab 192k -f mp3 "$TMPDIR"/audio3.mp3
 ################################################################################
-FILE="$(tr ' ' '-' <<< $1)"
-FILE="$(sed 's/--/-/g' <<< $FILE)"
-FILE="$(sed 's/--/-/g' <<< $FILE)"
+FILE="$(printf '%s' "$1" | tr ' ' '-' )"
+FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
 ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 "$TMPDIR"/audio3.mp3
 ffmpeg -i "$TMPDIR"/audio3.mp3 -c:v copy -ac 1 "$TMPDIR"/mono-output.mp3 && rm -f "$TMPDIR"/audio3.mp3
-mv "$TMPDIR"/mono-output.mp3 ${FILE%.*}-mono.mp3
+mv "$TMPDIR"/mono-output.mp3 "${FILE%.*}"-mono.mp3
 # TermuxArch/3dTermuxArch/scripts/tomono.sh FE
