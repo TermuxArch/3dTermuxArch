63f9fbb161d1b9f9187dc686a7d597f8142d7de6
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-02-04 19:00:32 -0500

    commit 936356637 on 20220204

--- scripts/tomono.sh
+++ scripts/tomono.sh
@@ -2,9 +2,28 @@
 # Copyright 2022 (c) by SDRausty, all rights reserved, see LICENSE
 # ffmpeg -i "$1" -vn -ar 11025 -ac 2 -ab 192k -f mp3 m11025.mp3
 ################################################################################
+set -eux
+[ -n "${1:-}" ] || { printf '%s\n' "Please enter a file name;  Exiting..." ; exit 69 ; }
+if [ ! -d audio/steo ] || [ ! -d audio/mono ] || [ ! -d audio/mono22050 ] || [ ! -d audio/mono11025 ]
+then
+mkdir -p audio/steo audio/mono audio/mono22050 audio/mono11025
+fi
+_PRNTMESG_ () { printf '%s\n' "Signal $1 received;  Continuing..." ; }
+# create file name based on input file nane
 FILE="$(printf '%s' "$1" | tr ' ' '-' )"
+FILE="$(printf '%s' "$FILE" | sed 's/(//g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/)//g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/\[//g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/\]//g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/{//g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/}//g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/official//i' )" # case insensitive
+FILE="$(printf '%s' "$FILE" | sed 's/video//i' )" # case insensitive
+FILE="$(printf '%s' "$FILE" | sed 's/_/-/g' )"
 FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
 FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
-ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 "${FILE%.*}".mp3
-ffmpeg -i "${FILE%.*}".mp3 -c:v copy -ac 1 "${FILE%.*}"-mono.mp3
-# TermuxArch/3dTermuxArch/scripts/tomono.sh FE
+ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 audio/steo/"${FILE%.*}".mp3 || _PRNTMESG_ 202
+ffmpeg -i audio/steo/"${FILE%.*}".mp3 -c:v copy -ac 1 audio/mono/"${FILE%.*}"-mono.mp3 || _PRNTMESG_ 204
+ffmpeg -i audio/mono/"${FILE%.*}"-mono.mp3 -vn -ar 22050 audio/mono22050/"${FILE%.*}"-m22050.mp3 || _PRNTMESG_ 206
+ffmpeg -i audio/mono/"${FILE%.*}"-mono.mp3 -vn -ar 11025 audio/mono11025/"${FILE%.*}"-m11025.mp3 || _PRNTMESG_ 208
+# TermuxArch/3dTermuxArch/scripts/tomono.sh _MKDIRS_() { mkdir -p {
