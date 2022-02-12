da7e386bf9b5a5230774f18c32dd6921904ef494
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-02-12 14:02:10 -0500

    commit 213187637 on 20220212

--- scripts/cvrt2mp3.sh
+++ scripts/cvrt2mp3.sh
@@ -2,7 +2,8 @@
 # Copyright 2022 (c) by SDRausty, all rights reserved, see LICENSE
 # Converts video and music files into mp3 audio format.
 ################################################################################
-set -eux
+set -eu
+printf '%s\n' "${0##*/} begun"
 [ -n "${1:-}" ] || { printf '%s\n' "Please enter a file name;  Exiting..." ; exit 69 ; }
 if [ ! -d audio/stereo ] || [ ! -d audio/mono ] || [ ! -d audio/mono22050 ] || [ ! -d audio/mono11025 ]
 then
@@ -25,8 +26,9 @@ FILE="$(printf '%s' "$FILE" | sed 's/video//i' )" # case insensitive
 FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
 FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
 FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
-ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 audio/stereo/"${FILE%.*}".mp3 || _PRNTMESG_ 202
+ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 audio/stereo/"${FILE%.*}".mp3 || { { pc ffmpeg || pci ffmpeg ; } && ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 audio/stereo/"${FILE%.*}".mp3 ; } || _PRNTMESG_ 202
 ffmpeg -i audio/stereo/"${FILE%.*}".mp3 -c:v copy -ac 1 audio/mono/"${FILE%.*}"-mono.mp3 || _PRNTMESG_ 204
 ffmpeg -i audio/mono/"${FILE%.*}"-mono.mp3 -vn -ar 22050 audio/mono22050/"${FILE%.*}"-m22050.mp3 || _PRNTMESG_ 206
 ffmpeg -i audio/mono/"${FILE%.*}"-mono.mp3 -vn -ar 11025 audio/mono11025/"${FILE%.*}"-m11025.mp3 || _PRNTMESG_ 208
-# TermuxArch/3dTermuxArch/scripts/cvrt2mp3.sh EF
+printf '%s\n' "${0##*/} done"
+# TermuxArch/3dTermuxArch/scripts/cvrt2mp3.sh FE
