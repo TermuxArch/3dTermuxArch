d34ea2bf77855de2becaebd80359a490d4f2d06d
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-06-03 16:19:08 -0400

    commit 20220603.95f38886

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
+<link rel="prerender" href="https://termuxarch.github.io/3dTermuxArch/">
 ## 3dTermuxArch
 
 [Sample haskell music output.mp4](https://user-images.githubusercontent.com/27742457/151226841-d11e98af-75a8-41c4-8862-fb2fa219e034.mp4)
 
-<!-- ##### -r--r--r-- [setupTermuxArch](https://raw.githubusercontent.com/TermuxArch/TermuxArch/master/setupTermuxArch)
-##### -rwxrwxrwx [setupTermuxArch](https://TermuxArch.github.io/TermuxArch/setupTermuxArch) -->
-
-<!-- [ghcup.music.mp3](https://raw.githubusercontent.com/TermuxArch/3dTermuxArch/master/mono22050/ghcup.music.mp3) -->
+![Set_Sound](https://raw.githubusercontent.com/TermuxArch/3dTermuxArch/master/setsound.png)
 
 <!-- TermuxArch/3dTermuxArch README.md FE -->
--- scripts/cvrt2mp3.sh
+++ scripts/cvrt2mp3.sh
@@ -4,12 +4,13 @@
 ################################################################################
 set -eu
 printf '%s\n' "${0##*/} begun"
+[ -x /usr/bin/ffmpeg ] || { pc ffmpeg || pci ffmpeg ; } || _PRNTMESG_ 200
 [ -n "${1:-}" ] || { printf '%s\n' "Please enter a file name;  Exiting..." ; exit 69 ; }
 if [ ! -d audio/stereo ] || [ ! -d audio/mono ] || [ ! -d audio/mono22050 ] || [ ! -d audio/mono11025 ]
 then
 mkdir -p audio/stereo audio/mono audio/mono22050 audio/mono11025
 fi
-_PRNTMESG_ () { printf '%s\n' "Signal $1 received;  Continuing..." ; }
+_PRNTMESG_ () { printf '%s\n' "Signal $1 received via ${0##*/};  Continuing..." ; }
 # create mp3 file name based on input file name
 FILE="$(printf '%s' "$1" | tr "'" '-')"
 FILE="$(printf '%s' "$FILE" | sed 's/ft\./ft/g' )"
@@ -27,7 +28,7 @@ FILE="$(printf '%s' "$FILE" | sed 's/video//i' )" # case insensitive
 FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
 FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
 FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
-ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 audio/stereo/"${FILE%.*}".mp3 || { { pc ffmpeg || pci ffmpeg ; } && ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 audio/stereo/"${FILE%.*}".mp3 ; } || _PRNTMESG_ 202
+ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 audio/stereo/"${FILE%.*}".mp3 || _PRNTMESG_ 202
 ffmpeg -i audio/stereo/"${FILE%.*}".mp3 -c:v copy -ac 1 audio/mono/"${FILE%.*}"-mono.mp3 || _PRNTMESG_ 204
 ffmpeg -i audio/mono/"${FILE%.*}"-mono.mp3 -vn -ar 22050 audio/mono22050/"${FILE%.*}"-m22050.mp3 || _PRNTMESG_ 206
 ffmpeg -i audio/mono/"${FILE%.*}"-mono.mp3 -vn -ar 11025 audio/mono11025/"${FILE%.*}"-m11025.mp3 || _PRNTMESG_ 208
