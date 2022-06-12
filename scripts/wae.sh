ff6c28373071aa13439b0d1e2bf045f9992dc102
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-06-12 13:08:19 -0400

    modified:   scripts/wae.sh

--- scripts/wae.sh
+++ scripts/wae.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
-am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService || printf '%s\n' "${0##*/} SIGNAL am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService;  Continuing..."
+set -u
+ARG0="${0##*/}"
+ARG1="${1:-no file name was provided}"
 ARG2="${2:-16}"
 ARG3="${3:-256}"
+am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService || printf '%s\n' "${ARG0^^} SIGNAL am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService;  Continuing..."
 _PLAYAUDIO_(){
 while true
 do
-printf '%s\n' "Playing ${1##*/}..."
-play-audio "${1:-no file name was provided}"
+printf '%s\n' "Playing $ARG1..."
+play-audio "$ARG1"
 SHFNNT="$(shuf -n 1 -i $ARG2-$ARG3)"
 printf '%s\n' "Snoozing for ${SHFNNT:-24} seconds..."
 sleep "${SHFNNT:-24}"
