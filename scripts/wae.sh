318ec01b2f216d6970606953bf8cb2a4307ed34a
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-06-25 12:31:34 -0400

    modified:   scripts/wae.sh

--- scripts/wae.sh
+++ scripts/wae.sh
@@ -14,6 +14,7 @@ ARG8="${8:-NO FILE NAME WAS GIVEN}"
 ARG9="${9:-NO FILE NAME WAS GIVEN}"
 [[ "$ARG2" =~ ^[0-9]+$ ]] || { printf '%s\n' "${ARG0^^} SIGNAL the second argument must be a number;  EXITING..." && exit ; }
 [[ "$ARG3" =~ ^[0-9]+$ ]] || { printf '%s\n' "${ARG0^^} SIGNAL the third argument must be a number;  EXITING..." && exit ; }
+[[ "$ARG2" -lt "$ARG3" ]] || { printf '%s\n' "${ARG0^^} SIGNAL the third argument must be less than the second argument;  EXITING..." && exit ; }
 _DPLY_(){
 printf '%s\n' "Playing '${TRCK##*/}'..."
 play-audio "$TRCK" || printf '%s\n' "${ARG0^^} SIGNAL play-audio $TRCK;  CONTINUING..."
@@ -27,10 +28,10 @@ while true
 do
 for TRCK in "$ARG1" "$ARG4" "$ARG5" "$ARG6" "$ARG7" "$ARG8" "$ARG9"
 do
-{ [[ "$TRCK"  == "NO FILE NAME WAS GIVEN" ]] && printf '%s\n' "${ARG0^^} SIGNAL NO FILE NAME WAS GIVEN;  EXITING..." && exit ; } || _DPLY_ "$TRCK"
+{ [[ "$TRCK"  == "NO FILE NAME WAS GIVEN" ]] && printf '%s\n' "${ARG0^^} SIGNAL no file name was given;  CONTINUING..." ; } || _DPLY_ "$TRCK"
 done
 done
 }
 [ -n "${1:-}" ] && { { [[ "${1//-}" = [Cc] ]] || [[ "${1//-}" = [Cc][Aa] ]] || [[ "${1//-}" = [Cc][Aa][Tt] ]] || [[ "${1//-}" = [Hh] ]] || [[ "${1//-}" = [Hh][Ee] ]] || [[ "${1//-}" = [Hh][Ee][Ll] ]] || [[ "${1//-}" = [Hh][Ee][Ll][Pp] ]] || [[ "${1//-}" = [Vv] ]]  || [[ "${1//-}" = [Vv][Ee] ]]  || [[ "${1//-}" = [Vv][Ee][Rr] ]]  || [[ "${1//-}" = [Vv][Ee][Rr][Ss] ]]  || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii] ]]  || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii][Oo] ]]  || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii][Oo][Nn] ]]  ; } && { printf '\e[0;32m%s\n' "${ARG0^^} INFO cat $0;  CONTINUING..." && cat "$0" ; exit ; } ; }
-{ [[ "$ARG1"  == "NO FILE NAME WAS GIVEN" ]] && printf '%s\n' "${ARG0^^} SIGNAL NO FILE NAME WAS GIVEN;  EXITING..." && exit ; } || _PLYD_ "$@"
+{ [[ "$ARG1"  == "NO FILE NAME WAS GIVEN" ]] && printf '%s\n' "${ARG0^^} SIGNAL no file name was given;  EXITING..." && exit ; } || _PLYD_ "$@"
 # wae EF
