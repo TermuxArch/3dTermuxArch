204cb8d38cb96ab8607e5c99d23d2b81f24099ce
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-06-25 12:07:34 -0400

    modified:   scripts/wae.sh

--- scripts/wae.sh
+++ scripts/wae.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Copyright 2022 by TermuxArch. All rights reserved, please see LICENSE 🌎 🌍 🌏
 ##############################################################################
-set -eux
+set -eu
 ARG0="${0##*/}"
 ARG1="${1:-NO FILE NAME WAS GIVEN}"
 ARG2="${2:-32}"
@@ -23,12 +23,12 @@ _PLYD_(){
 { am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService 1>/dev/null && printf '%s\n' "${ARG0^^} INFO am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService;  CONTINUING..." ; } || printf '%s\n' "${ARG0^^} SIGNAL am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService;  CONTINUING..."
 while true
 do
-for TRCK in "$ARG1" "$ARG2" "$ARG3" "$ARG4" "$ARG5" "$ARG6" "$ARG7" "$ARG8" "$ARG9"
+for TRCK in "$ARG1" "$ARG4" "$ARG5" "$ARG6" "$ARG7" "$ARG8" "$ARG9"
 do
-_DPLY_ "$TRCK"
+[[ "$TRCK"  == "NO FILE NAME WAS GIVEN" ]] || _DPLY_ "$TRCK"
 done
 done
 }
-[ -n "${1:-}" ] && { { [[ "${1//-}" = [Cc] ]] || [[ "${1//-}" = [Cc][Aa] ]] || [[ "${1//-}" = [Cc][Aa][Tt] ]] || [[ "${1//-}" = [Hh] ]] || [[ "${1//-}" = [Hh][Ee] ]] || [[ "${1//-}" = [Hh][Ee][Ll] ]] || [[ "${1//-}" = [Hh][Ee][Ll][Pp] ]] ; } && { printf '\e[0;32m%s\n' "${ARG0^^} INFO cat $0;  CONTINUING..." && cat "$0" ; exit ; } ; }
+[ -n "${1:-}" ] && { { [[ "${1//-}" = [Cc] ]] || [[ "${1//-}" = [Cc][Aa] ]] || [[ "${1//-}" = [Cc][Aa][Tt] ]] || [[ "${1//-}" = [Hh] ]] || [[ "${1//-}" = [Hh][Ee] ]] || [[ "${1//-}" = [Hh][Ee][Ll] ]] || [[ "${1//-}" = [Hh][Ee][Ll][Pp] ]] || [[ "${1//-}" = [Vv] ]]  || [[ "${1//-}" = [Vv][Ee] ]]  || [[ "${1//-}" = [Vv][Ee][Rr] ]]  || [[ "${1//-}" = [Vv][Ee][Rr][Ss] ]]  || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii] ]]  || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii][Oo] ]]  || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii][Oo][Nn] ]]  ; } && { printf '\e[0;32m%s\n' "${ARG0^^} INFO cat $0;  CONTINUING..." && cat "$0" ; exit ; } ; }
 _PLYD_ "$@"
 # wae EF
