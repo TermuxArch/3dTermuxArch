6d3fda55eae04e75d7dd673be259a6e5ff9cae34
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-06-26 22:33:29 -0400

    commit 20220626.26fba816

--- scripts/wae.bash
+++ scripts/wae.bash
@@ -3,18 +3,14 @@
 ##############################################################################
 set -eu
 ARG0="${0##*/}"
-ARG1="${1:-NO FILE NAME WAS GIVEN}"
-ARG2="${2:-32}"
-ARG3="${3:-512}"
-ARG4="${4:-NO FILE NAME WAS GIVEN}"
-ARG5="${5:-NO FILE NAME WAS GIVEN}"
-ARG6="${6:-NO FILE NAME WAS GIVEN}"
-ARG7="${7:-NO FILE NAME WAS GIVEN}"
-ARG8="${8:-NO FILE NAME WAS GIVEN}"
-ARG9="${9:-NO FILE NAME WAS GIVEN}"
-[[ "$ARG2" =~ ^[0-9]+$ ]] || { printf '\e[0;32m%s\e[0;31mEXITING...\e[0m\n' "${ARG0^^} INFO the second argument is reserved for the minimum snooze time and must be a number;  " && exit ; }
-[[ "$ARG3" =~ ^[0-9]+$ ]] || { printf '\e[0;32m%s\e[0;31mEXITING...\e[0m\n' "${ARG0^^} INFO the third argument is reserved for the maximum snooze time and must be a number;  " && exit ; }
-[[ "$ARG3" -gt "$ARG2" ]] || { printf '\e[0;32m%s\e[0;31mEXITING...\e[0m\n' "${ARG0^^} INFO the third argument must be more than the second argument;  " && exit ; }
+for TMPVRBL in "$@"
+do
+[[ "$TMPVRBL" =~ ^[0-9]+$ ]] || SNGS+=($TMPVRBL )
+{ [[ "$TMPVRBL" =~ ^[0-9]+$ ]] && { { [[ -z "${ARG2:-}" ]] && ARG2="$TMPVRBL" ; } || { [[ -z "${ARG3:-}" ]] && ARG3="$TMPVRBL" ; } ; } ; }
+done
+ARG2="${ARG2:-32}"
+ARG3="${ARG3:-512}"
+[[ "$ARG3" -gt "$ARG2" ]] || { printf '\e[0;32m%s\e[0;31mEXITING...\e[0m\n' "${ARG0^^} INFO the first numerical argument provided must be less than the second numerical argument given;  " && exit ; }
 _DPLY_(){
 printf '%s\n' "Playing '${TRCK##*/}'..."
 play-audio "$TRCK" || printf '%s\e[0;32mCONTINUING...\e[0m\n' "${ARG0^^} SIGNAL play-audio $TRCK;  "
@@ -25,14 +21,14 @@ sleep "${SHFNNT:-24}"
 _PLYD_(){
 TMPCMD="am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService 1>/dev/null"
 { $TMPCMD && printf '\e[0;32m%sCONTINUING...\e[0m\n' "${ARG0^^} INFO $TMPCMD;  " ; } || printf '\e[0;32m%s\e[0;32mCONTINUING...\e[0m\n' "${ARG0^^} SIGNAL $TMPCMD;  "
-while true
+until { read -s -n 1 -t 0.01 INPUT && { [[ $INPUT = [Ee] ]] || [[ $INPUT = [Qq] ]] ; } && printf '\e[0;32m%s\e[0;31mEXITING...\e[0m\n' "${ARG0^^} INFO keypress $INPUT given;  " && break ; }
 do
-for TRCK in "$ARG1" "$ARG4" "$ARG5" "$ARG6" "$ARG7" "$ARG8" "$ARG9"
+for TRCK in ${SNGS[@]}
 do
-[[ "$TRCK"  == "NO FILE NAME WAS GIVEN" ]] || _DPLY_ "$TRCK"
+_DPLY_ "$TRCK"
 done
 done
 }
 [ -n "${1:-}" ] && { { [[ "${1//-}" = [Cc] ]] || [[ "${1//-}" = [Cc][Aa] ]] || [[ "${1//-}" = [Cc][Aa][Tt] ]] || [[ "${1//-}" = [Hh] ]] || [[ "${1//-}" = [Hh][Ee] ]] || [[ "${1//-}" = [Hh][Ee][Ll] ]] || [[ "${1//-}" = [Hh][Ee][Ll][Pp] ]] || [[ "${1//-}" = [Vv] ]] || [[ "${1//-}" = [Vv][Ee] ]] || [[ "${1//-}" = [Vv][Ee][Rr] ]] || [[ "${1//-}" = [Vv][Ee][Rr][Ss] ]] || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii] ]] || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii][Oo] ]] || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii][Oo][Nn] ]] ; } && { printf '\e[0;32m%s\e[0m\n' "${ARG0^^} INFO cat $0;  CONTINUING..." && cat "$0" ; exit ; } ; }
-{ [[ "$ARG1" == "NO FILE NAME WAS GIVEN" ]] && printf '\e[0;32m%s\e[0;31mEXITING...\e[0m\n' "${ARG0^^} INFO no file name was given;  " && exit ; } || _PLYD_
+{ [[ -z "${SNGS[@]}" ]] && printf '\e[0;32m%s\e[0;31mEXITING...\e[0m\n' "${ARG0^^} INFO no file name was given;  " && exit ; } || _PLYD_
 # wae.bash EF
