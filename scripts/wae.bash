3411ff155c539703a314038f4c08ede69dedbc97
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-06-29 16:27:08 -0400

    commit 20220629.5f6609b4

--- scripts/wae.bash
+++ scripts/wae.bash
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 ## Copyright 2022 by TermuxArch, all rights reserved, please see LICENSE 🌎 🌍 🌏
-## The command 'wait an event' is derived from the theories of grey music cognition and music as medicine and group discussions circa 2018 at Termux community channels before the SDRausty account was banned by Termux curators who appear to be doing the work of open source code squatters.  Please reference https://github.com/SDRausty/builtAPKs/issues/2 for more information about open source code squatting:
-## snooze time = time between plays in seconds
-## SYNTAX: 'wae [audio files] [min snooze time] [max snooze time]'
-## USAGE: 'wae *.mp3 16 256'
+## The command 'wait an event' is derived from the theories of grey music cognition and music as medicine.  Termux group discussions circa 2018 at Termux community channels were very helpful before the SDRausty account was banned by Termux curators who are doing the work of open source code squatters.  Please reference https://github.com/SDRausty/builtAPKs/issues/2 for more information about open source code squatting.
+##
+## snooze time = time between each play in seconds
+##
+## SYNTAX: 'wae [audio files] [audio files] [min snooze time] [max snooze time]'
+## USAGE: 'wae *.mp3 path/file.mp4 16 256' plays sound files and waits from 16 to 256 seconds between each play.  Multiple Termux sessions can be used in order to run commands 'wae arguments' simultainiously and to switch seamlessly between plays.
 ##############################################################################
 set -eu
 ARG0="${0##*/}"
 [ "${1:-}" != "" ] && { { [[ "${1//-}" = [Cc] ]] || [[ "${1//-}" = [Cc][Aa] ]] || [[ "${1//-}" = [Cc][Aa][Tt] ]] || [[ "${1//-}" = [Vv] ]] || [[ "${1//-}" = [Vv][Ee] ]] || [[ "${1//-}" = [Vv][Ee][Rr] ]] || [[ "${1//-}" = [Vv][Ee][Rr][Ss] ]] || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii] ]] || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii][Oo] ]] || [[ "${1//-}" = [Vv][Ee][Rr][Ss][Ii][Oo][Nn] ]] ; } && { printf '\e[0;32m%s\e[0;31m  EXITING...\e[0m\n' "${ARG0^^} INFO cat $0;" && cat "$0" ; exit ; } ; }
-[ "${1:-}" != "" ] && { { [[ "${1//-}" = [Hh] ]] || [[ "${1//-}" = [Hh][Ee] ]] || [[ "${1//-}" = [Hh][Ee][Ll] ]] || [[ "${1//-}" = [Hh][Ee][Ll][Pp] ]] ; } && { TMPCMD="$(sed -n '2,6p' $0 | sed 's/##\ //g')" && printf '\e[0;32m%s\e[0;31m  EXITING...\e[0m\n' "${ARG0^^} INFO $TMPCMD;" ; exit ; } ; }
+[ "${1:-}" != "" ] && { { [[ "${1//-}" = [Hh] ]] || [[ "${1//-}" = [Hh][Ee] ]] || [[ "${1//-}" = [Hh][Ee][Ll] ]] || [[ "${1//-}" = [Hh][Ee][Ll][Pp] ]] ; } && { TMPCMD="$(sed -n '2,8p' $0 | sed 's/##\ //g')" && printf '\e[0;32m%s\e[0;31m  EXITING...\e[0m\n' "${ARG0^^} HELP $TMPCMD" ; exit ; } ; }
 for TMPVRBL in "$@"
 do
 [[ "$TMPVRBL" =~ ^[0-9]+$ ]] || SNGS+=("$TMPVRBL" )
