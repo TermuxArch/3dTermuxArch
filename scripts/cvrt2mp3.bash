a71d985e71156af37014eb9e6b42628970d6700f
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-10-11 20:30:31 -0400

    new file:   audio/mono/Country-Proud-Nu-Breed-&-Jesse-Howardmp4-mono.mp3 	new file:   audio/mono/Jesse-Howard-Little-Boy-and-The-Preacher-Manmp4-mono.mp3 	new file:   audio/mono/Nu-Breed-&-Jesse-Howard-Hailstormmp4-mono.mp3 	new file:   audio/mono/Nu-Breed-Feat-Jesse-Howard-Welcome-To-My-Housemp4-mono.mp3 	new file:   audio/mono/Welcome-to-My-Housemp4-mono.mp3 	new file:   audio/mono11025/Country-Proud-Nu-Breed-&-Jesse-Howardmp4-m11025.mp3 	new file:   audio/mono11025/Jesse-Howard-Little-Boy-and-The-Preacher-Manmp4-m11025.mp3 	new file:   audio/mono11025/Nu-Breed-&-Jesse-Howard-Hailstormmp4-m11025.mp3 	new file:   audio/mono11025/Nu-Breed-Feat-Jesse-Howard-Welcome-To-My-Housemp4-m11025.mp3 	new file:   audio/mono11025/Welcome-to-My-Housemp4-m11025.mp3 	new file:   audio/mono22050/Country-Proud-Nu-Breed-&-Jesse-Howardmp4-m22050.mp3 	new file:   audio/mono22050/Jesse-Howard-Little-Boy-and-The-Preacher-Manmp4-m22050.mp3 	new file:   audio/mono22050/Nu-Breed-&-Jesse-Howard-Hailstormmp4-m22050.mp3 	new file:   audio/mono22050/Nu-Breed-Feat-Jesse-Howard-Welcome-To-My-Housemp4-m22050.mp3 	new file:   audio/mono22050/Welcome-to-My-Housemp4-m22050.mp3 	new file:   audio/stereo/Country-Proud-Nu-Breed-&-Jesse-Howardmp4.mp3 	new file:   audio/stereo/Jesse-Howard-Little-Boy-and-The-Preacher-Manmp4.mp3 	new file:   audio/stereo/Nu-Breed-&-Jesse-Howard-Hailstormmp4.mp3 	new file:   audio/stereo/Nu-Breed-Feat-Jesse-Howard-Welcome-To-My-Housemp4.mp3 	new file:   audio/stereo/Welcome-to-My-Housemp4.mp3 	renamed:    scripts/cvrt2mp3.sh -> scripts/cvrt2mp3.bash

---
+++ scripts/cvrt2mp3.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Copyright 2022 (c) by SDRausty, all rights reserved, see LICENSE
+# Converts video and music files into mp3 audio format.
+################################################################################
+set -eu
+printf '%s\n' "${0##*/} begun"
+echo ${username2:-`whoami`}
+[ -x ${PREFIX:-/usr}/bin/ffmpeg ] || { pc ffmpeg || pci ffmpeg ; } || _PRNTMESG_ 200
+[ -n "${1:-}" ] || { printf '%s\n' "Please enter a file name;  Exiting..." ; exit 69 ; }
+if [ ! -d audio/stereo ] || [ ! -d audio/mono ] || [ ! -d audio/mono22050 ] || [ ! -d audio/mono11025 ]
+then
+mkdir -p audio/stereo audio/mono audio/mono22050 audio/mono11025
+fi
+_PRNTMESG_ () { printf '%s\n' "Signal $1 received via ${0##*/};  Continuing..." ; }
+# create mp3 file name based on input file name
+FILE="$(printf '%s' "$1" | tr "'" '-')"
+FILE="$(printf '%s' "$FILE" | sed 's/ft\./ft/g' )"
+FILE="$(printf '%s' "$FILE" | tr ' ' '-' )"
+FILE="$(printf '%s' "$FILE" | tr '(' '-' )"
+FILE="$(printf '%s' "$FILE" | tr ')' '-' )"
+FILE="$(printf '%s' "$FILE" | tr "[" '-')"
+FILE="$(printf '%s' "$FILE" | tr ']' '-' )"
+FILE="$(printf '%s' "$FILE" | tr '{' '-' )"
+FILE="$(printf '%s' "$FILE" | tr '}' '-' )"
+FILE="$(printf '%s' "$FILE" | tr '_' '-' )"
+FILE="$(printf '%s' "$FILE" | sed 's/official//i' )" # case insensitive
+FILE="$(printf '%s' "$FILE" | sed 's/please-read-description//i' )" # case insensitive
+FILE="$(printf '%s' "$FILE" | sed 's/video//i' )" # case insensitive
+FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
+FILE="$(printf '%s' "$FILE" | sed 's/--/-/g' )"
+FILE="${FILE//.}"
+FILE="${FILE//,}"
+FILE="${FILE//\~}"
+FILE="${FILE//--/-}"
+ffmpeg -i "$1" -vn -ac 2 -ab 192k -f mp3 audio/stereo/"${FILE%.*}".mp3 || _PRNTMESG_ 202
+ffmpeg -i audio/stereo/"${FILE%.*}".mp3 -c:v copy -ac 1 audio/mono/"${FILE%.*}"-mono.mp3 || _PRNTMESG_ 204
+ffmpeg -i audio/mono/"${FILE%.*}"-mono.mp3 -vn -ar 22050 audio/mono22050/"${FILE%.*}"-m22050.mp3 || _PRNTMESG_ 206
+ffmpeg -i audio/mono/"${FILE%.*}"-mono.mp3 -vn -ar 11025 audio/mono11025/"${FILE%.*}"-m11025.mp3 || _PRNTMESG_ 208
+printf '%s\n' "${0##*/} done"
+# TermuxArch/3dTermuxArch/scripts/cvrt2mp3.sh FE
